d9bbd43ea403adadaa989feef1f6cbe0fbf0087f
Tom <>
2025-04-19 21:17:58 +0200

    linter changes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,11 +6,17 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+	baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+	...compat.extends("next/core-web-vitals", "next/typescript"),
+	{
+		// Define your custom rules here
+		rules: {
+			"@typescript-eslint/no-unused-vars": "warn", // Change this to warning
+		},
+	},
 ];
 
 export default eslintConfig;
