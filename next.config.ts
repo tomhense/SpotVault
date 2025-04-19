5d7a6bad0aaa0e8a40e890cae4417ccdc42a0cc6
Tom <>
2025-04-19 21:36:16 +0200

    Still not working

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
 };
+
+if (process.env.NODE_ENV === "development") {
+	await setupDevPlatform();
+}
+
+export const runtime = "edge";
 
 export default nextConfig;
