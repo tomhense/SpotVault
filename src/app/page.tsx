f8e67a71774e87e74b65ac68fea2d2487d5adfe4
Tom <>
2025-04-22 18:48:50 +0200

    Changed some things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { Button, Container, Typography, Paper, Box } from "@mui/material";
 import { currentToken, tryFetchAndHandleAuthCode, redirectToSpotifyAuthorize, tryRefreshToken } from "@/utils/spotify_auth";
 import { NoSsr } from "@mui/material";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
 	const [isLoggedIn, setIsLoggedIn] = React.useState(false);
@@ -21,46 +22,49 @@ export default function Home() {
 	});
 
 	return (
-		<Container
-			maxWidth="lg"
-			sx={{
-				display: "flex",
-				flexDirection: "column",
-				alignItems: "center",
-				justifyContent: "center",
-				minHeight: "100vh",
-				backgroundColor: "grey.100",
-			}}
-		>
-			<Paper
-				elevation={3}
+		<Box sx={{ backgroundColor: "grey.100", minHeight: "100vh" }}>
+			<Navbar />
+			<Container
+				maxWidth="lg"
 				sx={{
-					padding: 4,
 					display: "flex",
 					flexDirection: "column",
 					alignItems: "center",
+					justifyContent: "center",
+					minHeight: "100vh",
+					backgroundColor: "grey.100",
 				}}
 			>
-				<Typography variant="h2" component="h1" sx={{ marginBottom: 4 }} gutterBottom>
-					SpotVault
-				</Typography>
-				<NoSsr>
-					{isLoggedIn ? (
-						<Box sx={{ display: "flex", gap: 2 }}>
-							<Button variant="outlined" component={Link} href="/backup" size="large">
-								Backup
+				<Paper
+					elevation={3}
+					sx={{
+						padding: 4,
+						display: "flex",
+						flexDirection: "column",
+						alignItems: "center",
+					}}
+				>
+					<Typography variant="h2" component="h1" sx={{ marginBottom: 4 }} gutterBottom>
+						SpotVault
+					</Typography>
+					<NoSsr>
+						{isLoggedIn ? (
+							<Box sx={{ display: "flex", gap: 2 }}>
+								<Button variant="outlined" component={Link} href="/backup" size="large">
+									Backup
+								</Button>
+								<Button variant="outlined" component={Link} href="/restore" size="large">
+									Restore
+								</Button>
+							</Box>
+						) : (
+							<Button variant="contained" color="primary" size="large" onClick={redirectToSpotifyAuthorize}>
+								Login
 							</Button>
-							<Button variant="outlined" component={Link} href="/restore" size="large">
-								Restore
-							</Button>
-						</Box>
-					) : (
-						<Button variant="contained" color="primary" size="large" onClick={redirectToSpotifyAuthorize}>
-							Login
-						</Button>
-					)}
-				</NoSsr>
-			</Paper>
-		</Container>
+						)}
+					</NoSsr>
+				</Paper>
+			</Container>
+		</Box>
 	);
 }
